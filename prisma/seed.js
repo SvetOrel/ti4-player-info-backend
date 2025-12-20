cda9dadd3e13057ef6d310e3ffbcdf1f00ed99cc
Sveta Grossman <70972468+SvetOrel@users.noreply.github.com>
2025-12-20 19:08:57 +0200

    fix

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 async function main() {
     await prisma.gamePlayerFaction.deleteMany();
